5e5f26cedaa43fb02374ee494839ca2d604a1668
kevincliff-pomar <kevincliff.pomar@gmail.com>
2018-07-28 17:32:01 +0800

    Lab Exercise 03 commit

---
+++ app/src/main/java/com/pomar/a03labex1/DisplayMessageActivity.java
@@ -0,0 +1,24 @@
+package com.pomar.a03labex1;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DisplayMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+        TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);
+        setContentView(textView);
+
+    }
+}
